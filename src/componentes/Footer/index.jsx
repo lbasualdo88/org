2acaf69cf18c37d2869c7bf6117052b4a76f29e6
Leonardo Basualdo <lbasualdo88@gmail.com>
2023-08-20 18:05:47 -0300

    Actualizacion de footer

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -5,18 +5,18 @@ const Footer = ()=> {
  
     return <footer className='footer' style={{backgroundImage:"url(/images/rectangle-1.svg)"}}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com'>
+            <a href='/'>
                 <img src="/images/facebook.png" alt="facebook" />
             </a>
-            <a href='https://www.aluracursos.com'>
+            <a href='/'>
                 <img src="/images/twitter.png" alt="twitter" />
             </a>
-            <a href='https://www.aluracursos.com'>
+            <a href='/'>
                 <img src="/images/instagram.png" alt="instagram" />
             </a>
         </div>
         <img src="/images/logo.png" alt='org' />
-        <strong>Desarrollado por Alura</strong>
+        <strong>Desarrollado por Leonardo Basualdo en conjunto con Alura</strong>
     </footer>   
 }
 
