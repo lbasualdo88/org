0699b7e8e7638286d3e7fa766adab6816ab0e2c3
Leonardo Basualdo <lbasualdo88@gmail.com>
2023-08-20 14:44:39 -0300

    foto org 1

--- src/componentes/Header/header.js
+++ src/componentes/Header/header.js
@@ -2,7 +2,7 @@ import "./header.css"
 
 function Header() {
     return <header className="header">
-        <img src="/public/img/HeaderHeader_total.png" alt="'Org"/>
+        <img src="/public/images/HeaderHeader_total.png" alt="'Org"/>
         </header>
 }
 
