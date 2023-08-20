355955d05807d5555ba5be1bd355dbda778754da
Leonardo Basualdo <lbasualdo88@gmail.com>
2023-08-20 15:28:02 -0300

    Actualizar img

--- src/componentes/Header/header.js
+++ src/componentes/Header/header.js
@@ -2,7 +2,7 @@ import "./header.css"
 
 function Header() {
     return <header className="header">
-        <img src="/public/images/HeaderHeader_total.png" alt="'Org"/>
+        <img src="/images/HeaderHeader_total.png" alt="'Org"/>
         </header>
 }
 
